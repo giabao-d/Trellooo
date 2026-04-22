2a12c4430f2828d2a8f9ba34a95f27315806ae6a
vuxnguyen <186534681+nguyenvux0@users.noreply.github.com>
2026-04-22 13:13:48 +0700

    Add files via upload

---
+++ GiaiPTBacNhat.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package baitap;
+
+import java.util.Scanner;
+
+public class GiaiPTBacNhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Nhap b: ");
+        double b = sc.nextDouble();
+
+        if (a != 0) {
+            double x = -b / a;
+            System.out.println("Phuong trinh " + a + "x + " + b + " = 0 có nghiem la: x = " + x);
+        } else {
+            if (b != 0) {
+                System.out.println("Phuong trinh " + a + "x + " + b + " = 0 vo nghiem");
+            } else {
+                System.out.println("Phuong trinh " + a + "x + " + b + " = 0 vo so nghiem");
+            }
+        }
+
+        sc.close();
+    }
+}
